2d5b95d08aee8885b48b94dd97821a6760119970
jesterr0 <jlw_4049@proton.me>
2025-01-18 16:26:04 -0500

    feat: add support for tvdb/anilist/mal_id

--- src/payloads/media_search.py
+++ src/payloads/media_search.py
@@ -9,6 +9,11 @@ class MediaSearchPayload:
     imdb_data: Movie | None = None
     tmdb_id: str | None = None
     tmdb_data: dict | None = None
+    tvdb_id: str | None = None
+    tvdb_data: dict | None = None
+    anilist_id: str | None = None
+    anilist_data: str | None = None
+    mal_id: str | None = None
     title: str | None = None
     year: int | None = None
     original_title: str | None = None
@@ -19,6 +24,11 @@ class MediaSearchPayload:
         self.imdb_data = None
         self.tmdb_id = None
         self.tmdb_data = None
+        self.tvdb_id = None
+        self.tvdb_data = None
+        self.anilist_id = None
+        self.anilist_data = None
+        self.mal_id = None
         self.title = None
         self.year = None
         self.original_title = None
