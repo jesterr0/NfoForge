e69800bf5cc405ac5acb26738205eb65c59a55f3
jesterr0 <jlw_4049@proton.me>
2025-01-18 16:25:41 -0500

    feat: media search back end now supports parsing tvdb api and anilist

--- src/backend/media_search.py
+++ src/backend/media_search.py
@@ -1,10 +1,16 @@
+import asyncio
 import concurrent.futures
+import re
 import requests
-from itertools import zip_longest
+import tvdb_v4_official
 
+from guessit import guessit
+from itertools import zip_longest
 from imdb import Cinemagoer
 from imdb.Movie import Movie
-from guessit import guessit
+from typing import Any
+from rapidfuzz import fuzz
+from unidecode import unidecode
 
 from src.enums.tmdb_genres import TMDBGenreIDsMovies, TMDBGenreIDsSeries
 from src.exceptions import MediaParsingError
@@ -176,8 +182,135 @@ class MediaSearchBackEnd:
         year = get_info.get("year", "")
         return title, year
 
+    async def parse_other_ids(
+        self,
+        imdb_id: str,
+        tmdb_title: str,
+        tmdb_year: int,
+        original_language: str,
+        tmdb_genres: list[TMDBGenreIDsMovies],
+        tvdb_api_key: str,
+    ) -> dict[str, Any]:
+        tasks = {
+            "imdb_data": asyncio.create_task(self.parse_imdb_data(imdb_id)),
+            "tvdb_data": asyncio.create_task(
+                self.parse_tvdb_data(imdb_id, tvdb_api_key)
+            ),
+        }
+
+        # parse anime if needed
+        if TMDBGenreIDsMovies.ANIMATION in tmdb_genres and original_language == "ja":
+            tasks["ani_list_data"] = asyncio.create_task(
+                self.parse_ani_list(tmdb_title, tmdb_year)
+            )
+
+        results = {}
+        for key, task in tasks.items():
+            try:
+                result = await task
+                results[key] = {"success": True, "result": result}
+            except Exception as e:
+                results[key] = {"success": False, "error": str(e)}
+        return results
+
     @staticmethod
-    def parse_imdb(imdb_id: str) -> Movie | None:
+    async def parse_imdb_data(imdb_id: str) -> Movie | None:
         imdb_parse = Cinemagoer()
         get_movie = imdb_parse.get_movie(imdb_id.replace("t", ""))
-        return get_movie if get_movie else None
+        if get_movie:
+            return get_movie
+
+    @staticmethod
+    async def parse_tvdb_data(imdb_id: str, api_key: str) -> dict | None:
+        tvdb_parse = tvdb_v4_official.TVDB(api_key)
+        data = tvdb_parse.search_by_remote_id(imdb_id)
+        if data and isinstance(data, list):
+            return data[0]
+
+    @staticmethod
+    async def parse_ani_list(tmdb_title: str, tmdb_year: int) -> dict[str, Any] | None:
+        matcher = MatchAnilistTitle(tmdb_title, tmdb_year)
+        best_match = await matcher.match()
+        if best_match:
+            # {'id': 21519, 'idMal': 32281, 'title': {'romaji': 'Kimi no Na wa.', 'english': 'Your Name.', 'native': '君の名は。'}, 'seasonYear': 2016, 'episodes': 1}
+            return best_match
+
+
+class MatchAnilistTitle:
+    def __init__(self, title: str, year: int) -> None:
+        self.title = title
+        self.year = year
+        self.data = None
+
+    @staticmethod
+    async def parse_ani_list(tmdb_title: str):
+        query = """
+            query ($search: String) {
+                Page (page: 1) {
+                    pageInfo {
+                        total
+                    }
+                    media (search: $search, type: ANIME, sort: SEARCH_MATCH) {
+                        id
+                        idMal
+                        title {
+                            romaji
+                            english
+                            native
+                        }
+                        seasonYear
+                        episodes
+                    }
+                }
+            }
+        """
+        variables = {"search": tmdb_title}
+        response = await asyncio.to_thread(
+            requests.post,
+            "https://graphql.anilist.co",
+            json={"query": query, "variables": variables},
+        )
+        response_json = response.json()
+        return response_json
+
+    def normalize_text(self, text: str) -> str:
+        """Normalize text by removing accents, lowercasing, and stripping non-alphanumeric characters."""
+        text = unidecode(text.lower())
+        return re.sub(r"[^a-z0-9]", "", text)
+
+    def filter_by_year(self, media: list, year: int) -> list:
+        """Filter media by the exact year."""
+        return [anime for anime in media if anime.get("seasonYear") == year]
+
+    def get_best_match(self, media: list, search_name: str) -> dict[str, Any] | None:
+        """Find the best match for a title using fuzzy string matching."""
+        search_name_normalized = self.normalize_text(search_name)
+        best_match: dict[str, Any] | None = None
+        highest_score = 0
+
+        for anime in media:
+            for _title_type, title in anime["title"].items():
+                if title:
+                    title_normalized = self.normalize_text(title)
+                    score = fuzz.ratio(search_name_normalized, title_normalized)
+
+                    if score > highest_score:
+                        highest_score = score
+                        best_match = anime
+
+        return best_match
+
+    async def match(self) -> dict | None:
+        """Match the anime title from the filtered data."""
+        # fetch the data asynchronously
+        response_data = await self.parse_ani_list(self.title)
+        self.data = response_data
+
+        # filter by year
+        filtered_media = self.filter_by_year(
+            self.data["data"]["Page"]["media"], self.year
+        )
+
+        # find best match
+        if filtered_media:
+            return self.get_best_match(filtered_media, self.title)
