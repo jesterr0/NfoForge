9eb55e1e50e049cc8c39f3bff1f22e3e0db37089
jesterr0 <jlw_4049@proton.me>
2025-01-24 19:10:20 -0500

    feat: update MediaInputBackEnd to handle multiple files or handle a sequence of files

--- src/backend/media_input.py
+++ src/backend/media_input.py
@@ -1,8 +1,19 @@
+from collections.abc import Sequence
 from pathlib import Path
 from pymediainfo import MediaInfo
 
 
 class MediaInputBackEnd:
-    def get_media_info(self, file_input: Path) -> MediaInfo | None:
+    __slots__ = ()
+
+    @staticmethod
+    def get_media_info(file_input: Path) -> MediaInfo | None:
         data = MediaInfo.parse(file_input)
         return data if isinstance(data, MediaInfo) else None
+
+    @staticmethod
+    def get_media_info_files(files: Sequence[Path]) -> list[MediaInfo]:
+        media_info_data = []
+        for media_file in files:
+            media_info_data.append(MediaInputBackEnd.get_media_info(media_file))
+        return media_info_data
