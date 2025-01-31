b0c268cda1ef3b12c87e0318f886968128b05c08
jesterr0 <jlw_4049@proton.me>
2025-01-30 22:47:25 -0500

    refactor: enum is now string based instead of numbered refactor: imports from `CaseInsensitiveEnum`

--- src/enums/image_host.py
+++ src/enums/image_host.py
@@ -1,20 +1,13 @@
-from enum import auto as auto_enum
+from typing_extensions import override
 from src.enums import CaseInsensitiveEnum
 
 
 class ImageHost(CaseInsensitiveEnum):
-    DISABLED = auto_enum()
-    CHEVERETO_V3 = auto_enum()
-    CHEVERETO_V4 = auto_enum()
-    IMAGE_BOX = auto_enum()
-    IMAGE_BB = auto_enum()
+    CHEVERETO_V3 = "Chevereto v3"
+    CHEVERETO_V4 = "Chevereto v4"
+    IMAGE_BOX = "ImageBox"
+    IMAGE_BB = "ImageBB"
 
+    @override
     def __str__(self) -> str:
-        img_host_map = {
-            ImageHost.DISABLED: "Disabled",
-            ImageHost.CHEVERETO_V3: "Chevereto v3",
-            ImageHost.CHEVERETO_V4: "Chevereto v4",
-            ImageHost.IMAGE_BOX: "ImageBox",
-            ImageHost.IMAGE_BB: "ImageBB",
-        }
-        return img_host_map[self]
+        return self.value
