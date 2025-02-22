f641be3b53e80057c6e261c36290bfc7179d9226
jesterr0 <jlw_4049@proton.me>
2025-02-22 00:14:50 -0500

    refactor: now passes if program is frozen or not

--- src/backend/utils/working_dir.py
+++ src/backend/utils/working_dir.py
@@ -1,25 +1,24 @@
 import sys
-from typing import Tuple
 from pathlib import Path
 
 
-def _get_working_directories() -> Tuple[Path, Path]:
+def _get_working_directories() -> tuple[Path, Path, bool]:
     """
     Used to determine the correct working directory automatically.
     This way we can utilize files/relative paths easily.
 
     Returns:
-        (Path, Path): Current working directory, runtime directory
+        (Path, Path, bool): Current working directory, runtime directory, frozen.
     """
     # we're in a pyinstaller bundle
     if getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS"):
         path = Path(sys.executable).parent
-        return path, path / "bundle" / "runtime"
+        return path, path / "bundle" / "runtime", True
 
     # we're running from a *.py file
     else:
         path = Path.cwd()
-        return path, path / "runtime"
+        return path, path / "runtime", False
 
 
-CURRENT_DIR, RUNTIME_DIR = _get_working_directories()
+CURRENT_DIR, RUNTIME_DIR, IS_FROZEN = _get_working_directories()
