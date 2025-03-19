27cf1949c7205866314e437742abeab84710f668
jesterr0 <jlw_4049@proton.me>
2025-03-18 22:42:35 -0400

    feat: create NamedTuple RenameNormalization

--- src/packages/custom_types.py
+++ src/packages/custom_types.py
@@ -1,5 +1,6 @@
 from typing import NamedTuple
 from src.enums.image_host import ImageHost, ImageSource
+from src.enums.rename import TypeHierarchy
 
 SubNames = NamedTuple("SubNames", [("source", str), ("encode", str)])
 CropValues = NamedTuple(
@@ -20,4 +21,13 @@ ImageUploadData = NamedTuple(
 ImageUploadFromTo = NamedTuple(
     "ImageUploadFromTo",
     [("img_from", ImageSource), ("img_to", ImageSource | ImageHost)],
+)
+RenameNormalization = NamedTuple(
+    "RenameNormalization",
+    (
+        ("normalized", str),
+        ("re_gex", tuple[str]),
+        ("period_formatted", str),
+        ("cut", TypeHierarchy),
+    ),
 )
