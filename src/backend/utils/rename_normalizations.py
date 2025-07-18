a54f1a35cd54772cfd4a8b6bec2c002194b5ac67
jesterr0 <jlw_4049@proton.me>
2025-07-18 18:33:13 -0400

    refactor: remove un-needed code

--- src/backend/utils/rename_normalizations.py
+++ src/backend/utils/rename_normalizations.py
@@ -1,6 +1,5 @@
 from collections.abc import Sequence
 
-from src.enums.rename import TypeHierarchy
 from src.packages.custom_types import RenameNormalization
 
 
@@ -8,107 +7,77 @@ EDITION_INFO: Sequence[RenameNormalization] = (
     RenameNormalization(
         "Alternative Cut",
         (r"alternative(?:[\s|\.]*cut)?",),
-        "Alternative.Cut.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Collectors Edition",
         (r"(?:collector's|collectors|collector)[\s|\.]*edition",),
-        "Collectors.Edition.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Criterion Edition",
         (r"criterion(?:[\s|\.]*edition)?",),
-        "Criterion.Edition.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Deluxe Edition",
         (r"deluxe(?:[\s|\.]*edition)?",),
-        "Deluxe.Edition.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Directors Cut",
         (r"(?:director's|directors)[\s|\.]*cut",),
-        "Directors.Cut.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Extended Cut",
         (r"extended(?:[\s|\.]*cut)?",),
-        "Extended.Cut.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Limited Edition",
         (r"limited(?:[\s|\.]*edition)?",),
-        "Limited.Edition.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Remastered",
         (r"remastered",),
-        "Remastered.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Special Edition",
         (r"special(?:[\s|\.]*edition)?",),
-        "Special.Edition.",
-        TypeHierarchy.CUT,
     ),
     RenameNormalization(
         "Theatrical Cut",
         (r"theatrical(?:[\s|\.]*cut)?",),
-        "Theatrical.Cut.",
-        TypeHierarchy.CUT,
     ),
-    RenameNormalization(
-        "Uncensored", (r"uncensored",), "Uncensored.", TypeHierarchy.CUT
-    ),
+    RenameNormalization("Uncensored", (r"uncensored",)),
     RenameNormalization(
         "Ultimate",
         (r"ultimate(?:[\s|\.]*edition)?",),
-        "Ultimate.",
-        TypeHierarchy.CUT,
     ),
-    RenameNormalization("Unrated", (r"unrated",), "Unrated.", TypeHierarchy.CUT),
-    RenameNormalization("Uncut", (r"uncut",), "Uncut.", TypeHierarchy.CUT),
+    RenameNormalization("Unrated", (r"unrated",)),
+    RenameNormalization("Uncut", (r"uncut",)),
 )
 
 # store edition strings only in a list to be used for quick matches
 EDITION_STRINGS_ONLY = [x.normalized.lower().split()[0].strip() for x in EDITION_INFO]
 
 FRAME_SIZE_INFO = (
-    RenameNormalization("IMAX", (r"imax",), "IMAX.", TypeHierarchy.EXTRAS),
+    RenameNormalization("IMAX", (r"imax",)),
     RenameNormalization(
         "Open Matte",
         (r"open[\s|\.]*matte",),
-        "Open.Matte.",
-        TypeHierarchy.EXTRAS,
     ),
 )
 
 LOCALIZATION_INFO = (
-    RenameNormalization("Dubbed", (r"dubbed",), "Dubbed.", TypeHierarchy.LOCALIZATION),
-    RenameNormalization("Subbed", (r"subbed",), "Subbed.", TypeHierarchy.LOCALIZATION),
+    RenameNormalization("Dubbed", (r"dubbed",)),
+    RenameNormalization("Subbed", (r"subbed",)),
 )
 
 RE_RELEASE_INFO = (
-    RenameNormalization(
-        "PROPER", (r"proper(?![2345])",), "PROPER.", TypeHierarchy.RE_RELEASE
-    ),
-    RenameNormalization("PROPER2", (r"proper2",), "PROPER2.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("PROPER3", (r"proper3",), "PROPER3.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("PROPER4", (r"proper4",), "PROPER4.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("PROPER5", (r"proper5",), "PROPER5.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization(
-        "REPACK", (r"repack(?![2345])",), "REPACK.", TypeHierarchy.RE_RELEASE
-    ),
-    RenameNormalization("REPACK2", (r"repack2",), "REPACK2.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("REPACK3", (r"repack3",), "REPACK3.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("REPACK4", (r"repack4",), "REPACK4.", TypeHierarchy.RE_RELEASE),
-    RenameNormalization("REPACK5", (r"repack5",), "REPACK5.", TypeHierarchy.RE_RELEASE),
+    RenameNormalization("PROPER", (r"proper(?![2345])",)),
+    RenameNormalization("PROPER2", (r"proper2",)),
+    RenameNormalization("PROPER3", (r"proper3",)),
+    RenameNormalization("PROPER4", (r"proper4",)),
+    RenameNormalization("PROPER5", (r"proper5",)),
+    RenameNormalization("REPACK", (r"repack(?![2345])",)),
+    RenameNormalization("REPACK2", (r"repack2",)),
+    RenameNormalization("REPACK3", (r"repack3",)),
+    RenameNormalization("REPACK4", (r"repack4",)),
+    RenameNormalization("REPACK5", (r"repack5",)),
 )
--- src/enums/rename.py
+++ src/enums/rename.py
@@ -1,25 +1,4 @@
-from enum import Enum, auto as auto_enum
-
-from typing_extensions import override
-
 from src.enums import CaseInsensitiveStrEnum
-
-
-class TypeHierarchy(Enum):
-    EXTRAS = auto_enum()
-    CUT = auto_enum()
-    LOCALIZATION = auto_enum()
-    RE_RELEASE = auto_enum()
-
-    @override
-    def __str__(self) -> str:
-        str_map = {
-            TypeHierarchy.EXTRAS: "Extras",
-            TypeHierarchy.CUT: "Cut",
-            TypeHierarchy.LOCALIZATION: "Localization",
-            TypeHierarchy.RE_RELEASE: "Re-Release",
-        }
-        return str_map[self]
 
 
 class QualitySelection(CaseInsensitiveStrEnum):
