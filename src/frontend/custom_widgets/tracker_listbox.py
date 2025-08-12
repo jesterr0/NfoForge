4ada41538ccd8eb3bdac6c466e8ed6d118bcffa0
jesterr0 <jlw_4049@proton.me>
2025-08-12 12:45:13 -0400

    feat: add support for ShareIsland fix: add missing opt in mod queue for aither and reelflix

--- src/frontend/custom_widgets/tracker_listbox.py
+++ src/frontend/custom_widgets/tracker_listbox.py
@@ -516,6 +516,9 @@ class RFTrackerEdit(TrackerEditBase):
         stream_optimized_lbl = QLabel("Stream Optimized", self)
         self.stream_optimized = QCheckBox(self)
 
+        opt_in_mod_queue_lbl = QLabel("Opt-in Mod Queue", self)
+        self.opt_in_mod_queue = QCheckBox(self)
+
         image_width_lbl = QLabel("Image Width", self)
         self.image_width = QSpinBox(self)
         self.image_width.setRange(300, 2000)
@@ -546,6 +549,7 @@ class RFTrackerEdit(TrackerEditBase):
         self.add_pair_to_layout(internal_lbl, self.internal)
         self.add_pair_to_layout(personal_release_lbl, self.personal_release)
         self.add_pair_to_layout(stream_optimized_lbl, self.stream_optimized)
+        self.add_pair_to_layout(opt_in_mod_queue_lbl, self.opt_in_mod_queue)
         self.add_pair_to_layout(image_width_lbl, self.image_width)
         self.add_widget_to_layout(
             staff_and_internal_lbl,
@@ -571,6 +575,7 @@ class RFTrackerEdit(TrackerEditBase):
         self.internal.setChecked(bool(tracker_data.internal))
         self.personal_release.setChecked(bool(tracker_data.personal_release))
         self.stream_optimized.setChecked(bool(tracker_data.stream_optimized))
+        self.opt_in_mod_queue.setChecked(bool(tracker_data.opt_in_to_mod_queue))
         self.image_width.setValue(tracker_data.image_width)
         self.featured.setChecked(bool(tracker_data.featured))
         self.free.setChecked(bool(tracker_data.free))
@@ -601,6 +606,9 @@ class RFTrackerEdit(TrackerEditBase):
         )
         self.config.cfg_payload.rf_tracker.stream_optimized = int(
             self.stream_optimized.isChecked()
+        )
+        self.config.cfg_payload.rf_tracker.opt_in_to_mod_queue = int(
+            self.opt_in_mod_queue.isChecked()
         )
         self.config.cfg_payload.rf_tracker.image_width = self.image_width.value()
         self.config.cfg_payload.rf_tracker.featured = int(self.featured.isChecked())
@@ -633,6 +641,9 @@ class AitherTrackerEdit(TrackerEditBase):
         stream_optimized_lbl = QLabel("Stream Optimized", self)
         self.stream_optimized = QCheckBox(self)
 
+        opt_in_mod_queue_lbl = QLabel("Opt-in Mod Queue", self)
+        self.opt_in_mod_queue = QCheckBox(self)
+
         image_width_lbl = QLabel("Image Width", self)
         self.image_width = QSpinBox(self)
         self.image_width.setRange(300, 2000)
@@ -663,6 +674,7 @@ class AitherTrackerEdit(TrackerEditBase):
         self.add_pair_to_layout(internal_lbl, self.internal)
         self.add_pair_to_layout(personal_release_lbl, self.personal_release)
         self.add_pair_to_layout(stream_optimized_lbl, self.stream_optimized)
+        self.add_pair_to_layout(opt_in_mod_queue_lbl, self.opt_in_mod_queue)
         self.add_pair_to_layout(image_width_lbl, self.image_width)
         self.add_widget_to_layout(
             staff_and_internal_lbl,
@@ -688,6 +700,7 @@ class AitherTrackerEdit(TrackerEditBase):
         self.internal.setChecked(bool(tracker_data.internal))
         self.personal_release.setChecked(bool(tracker_data.personal_release))
         self.stream_optimized.setChecked(bool(tracker_data.stream_optimized))
+        self.opt_in_mod_queue.setChecked(bool(tracker_data.opt_in_to_mod_queue))
         self.image_width.setValue(tracker_data.image_width)
         self.featured.setChecked(bool(tracker_data.featured))
         self.free.setChecked(bool(tracker_data.free))
@@ -720,6 +733,9 @@ class AitherTrackerEdit(TrackerEditBase):
         )
         self.config.cfg_payload.aither_tracker.stream_optimized = int(
             self.stream_optimized.isChecked()
+        )
+        self.config.cfg_payload.aither_tracker.opt_in_to_mod_queue = int(
+            self.opt_in_mod_queue.isChecked()
         )
         self.config.cfg_payload.aither_tracker.image_width = self.image_width.value()
         self.config.cfg_payload.aither_tracker.featured = int(self.featured.isChecked())
@@ -1023,6 +1039,96 @@ class DarkPeersEdit(TrackerEditBase):
             self.config.cfg_payload.darkpeers_tracker.row_space = row_space
 
 
+class ShareIslandEdit(TrackerEditBase):
+    def __init__(self, config: Config, parent=None) -> None:
+        super().__init__(config, parent)
+
+        api_key_lbl = QLabel("API Key", self)
+        self.api_key = MaskedQLineEdit(parent=self, masked=True)
+
+        anonymous_lbl = QLabel("Anonymous", self)
+        self.anonymous = QCheckBox(self)
+
+        internal_lbl = QLabel("Internal", self)
+        self.internal = QCheckBox(self)
+
+        personal_release_lbl = QLabel("Personal Release", self)
+        self.personal_release = QCheckBox(self)
+
+        opt_in_mod_queue_lbl = QLabel("Opt-in Mod Queue", self)
+        self.opt_in_mod_queue = QCheckBox(self)
+
+        image_width_lbl = QLabel("Image Width", self)
+        self.image_width = QSpinBox(self)
+        self.image_width.setRange(300, 2000)
+        self.image_width.wheelEvent = self._disable_scrollwheel_spinbox
+
+        self.add_pair_to_layout(api_key_lbl, self.api_key)
+        self.add_pair_to_layout(anonymous_lbl, self.anonymous)
+        self.add_pair_to_layout(internal_lbl, self.internal)
+        self.add_pair_to_layout(opt_in_mod_queue_lbl, self.opt_in_mod_queue)
+        self.add_pair_to_layout(personal_release_lbl, self.personal_release)
+        self.add_pair_to_layout(image_width_lbl, self.image_width)
+        self.add_screen_shot_settings()
+
+    def load_settings(self) -> None:
+        tracker_data = self.config.cfg_payload.shareisland_tracker
+        self.upload_enabled.setChecked(tracker_data.upload_enabled)
+        self.announce_url.setText(
+            tracker_data.announce_url if tracker_data.announce_url else ""
+        )
+        self.comments.setText(tracker_data.comments if tracker_data.comments else "")
+        self.source.setText(tracker_data.source if tracker_data.source else "")
+        self.api_key.setText(tracker_data.api_key if tracker_data.api_key else "")
+        self.anonymous.setChecked(bool(tracker_data.anonymous))
+        self.internal.setChecked(bool(tracker_data.internal))
+        self.personal_release.setChecked(bool(tracker_data.personal_release))
+        self.opt_in_mod_queue.setChecked(bool(tracker_data.opt_in_to_mod_queue))
+        self.image_width.setValue(tracker_data.image_width)
+        if self.screen_shot_settings:
+            self.screen_shot_settings.load_settings(
+                url_type=URLType(tracker_data.url_type),
+                columns=tracker_data.column_s,
+                col_space=tracker_data.column_space,
+                row_space=tracker_data.row_space,
+            )
+
+    def save_settings(self) -> None:
+        self.config.cfg_payload.shareisland_tracker.upload_enabled = (
+            self.upload_enabled.isChecked()
+        )
+        self.config.cfg_payload.shareisland_tracker.announce_url = (
+            self.announce_url.text().strip()
+        )
+        self.config.cfg_payload.shareisland_tracker.comments = (
+            self.comments.text().strip()
+        )
+        self.config.cfg_payload.shareisland_tracker.source = self.source.text().strip()
+        self.config.cfg_payload.shareisland_tracker.api_key = (
+            self.api_key.text().strip()
+        )
+        self.config.cfg_payload.shareisland_tracker.anonymous = int(
+            self.anonymous.isChecked()
+        )
+        self.config.cfg_payload.shareisland_tracker.internal = int(
+            self.internal.isChecked()
+        )
+        self.config.cfg_payload.shareisland_tracker.personal_release = int(
+            self.personal_release.isChecked()
+        )
+        self.config.cfg_payload.shareisland_tracker.opt_in_to_mod_queue = int(
+            self.opt_in_mod_queue.isChecked()
+        )
+        self.config.cfg_payload.shareisland_tracker.image_width = (
+            self.image_width.value()
+        )
+        if self.screen_shot_settings:
+            col_s, col_space, row_space = self.screen_shot_settings.current_settings()
+            self.config.cfg_payload.shareisland_tracker.column_s = col_s
+            self.config.cfg_payload.shareisland_tracker.column_space = col_space
+            self.config.cfg_payload.shareisland_tracker.row_space = row_space
+
+
 class TrackerListWidget(QWidget):
     def __init__(self, config: Config, parent=None) -> None:
         super().__init__(parent)
@@ -1086,6 +1192,8 @@ class TrackerListWidget(QWidget):
             tracker_widget = LSTTrackerEdit(self.config, self)
         elif tracker is TrackerSelection.DARK_PEERS:
             tracker_widget = DarkPeersEdit(self.config, self)
+        elif tracker is TrackerSelection.SHARE_ISLAND:
+            tracker_widget = ShareIslandEdit(self.config, self)
 
         if tracker_widget:
             tracker_widget.load_data.emit()
