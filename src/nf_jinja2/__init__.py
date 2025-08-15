51e3fce63aaafa4d8ec47f3d73edd6b3b6d45516
jesterr0 <jlw_4049@proton.me>
2025-08-15 12:01:41 -0400

    refactor: control resettable globals feat: add helper method to reset specific globals if needed outside of reset

--- src/nf_jinja2/__init__.py
+++ src/nf_jinja2/__init__.py
@@ -1,10 +1,11 @@
+from collections.abc import Sequence
 from typing import Any
 
 from jinja2 import Environment, FileSystemLoader, Template
 
 
 class Jinja2TemplateEngine:
-    __slots__ = ("environment", "_added_globals")
+    __slots__ = ("environment", "_resettable_globals")
 
     def __init__(self, template_dir: str | None = None, **env_options) -> None:
         """
@@ -13,21 +14,29 @@ class Jinja2TemplateEngine:
         :param template_dir: Directory containing templates (optional).
         :param env_options: Options to configure the Jinja2 Environment.
         """
-        self._added_globals = []
+        self._resettable_globals = []
         self.environment = Environment(
             loader=FileSystemLoader(template_dir) if template_dir else None,
             **env_options,
         )
 
-    def add_global(self, name: str, value: Any) -> None:
+    def add_global(self, name: str, value: Any, allow_reset: bool) -> None:
         """Add a global variable to the environment."""
         self.environment.globals[name] = value
-        self._added_globals.append(name)
+        if allow_reset:
+            self._resettable_globals.append(name)
 
     def reset_added_globals(self) -> None:
-        for item in self._added_globals:
+        for item in self._resettable_globals:
             self.environment.globals.pop(item, None)
-        self._added_globals.clear()
+        self._resettable_globals.clear()
+
+    def reset_specific_globals(self, global_names: Sequence[str]) -> None:
+        """Reset only specific globals by name."""
+        for name in global_names:
+            if name in self._resettable_globals:
+                self.environment.globals.pop(name, None)
+                self._resettable_globals.remove(name)
 
     def add_filter(self, name: str, func) -> None:
         """Add a custom filter to the environment."""
