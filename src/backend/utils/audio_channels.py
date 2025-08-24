336b6a3a77cb0c05a7542a16084c2157f9a73253
jesterr0 <jlw_4049@proton.me>
2025-08-23 21:12:32 -0400

    fix: type hints refactor: some variable names that makes sense

--- src/backend/utils/audio_channels.py
+++ src/backend/utils/audio_channels.py
@@ -1,13 +1,17 @@
 import re
-from pymediainfo import MediaInfo
+
+from pymediainfo import Track
+
 from src.enums.audio_channels import AudioChannels
 
 
 class ParseAudioChannels:
+    __slots__ = ()
+
     @staticmethod
-    def get_channel_layout(mi_obj: MediaInfo.parse) -> str:
-        num_channels = AudioChannels(ParseAudioChannels.get_channels(mi_obj)).value
-        channel_positions = ParseAudioChannels._get_channel_layout(mi_obj)
+    def get_channel_layout(a_track: Track) -> str:
+        num_channels = AudioChannels(ParseAudioChannels.get_channels(a_track)).value
+        channel_positions = ParseAudioChannels._get_channel_layout(a_track)
 
         if "LFE" in channel_positions:
             num_channels -= 1
@@ -15,24 +19,23 @@ class ParseAudioChannels:
         return f"{num_channels}.{'1' if 'LFE' in channel_positions else '0'}"
 
     @staticmethod
-    def _get_channel_layout(mi_obj: MediaInfo.parse) -> str:
+    def _get_channel_layout(a_track: Track) -> str:
         # TODO: with pcm/raw data we sometimes don't get a channel layout,
         # if this becomes a problem where invalid channel layouts are being
         # returned we'll need to prompt the user unless a better solution is
         # discovered.
         # We'll likely just prompt the user with possible channels
         channel_positions = ""
+        if a_track.channel_positions:
+            channel_positions = a_track.channel_positions
 
-        if mi_obj.channel_positions:
-            channel_positions = mi_obj.channel_positions
-
-        if mi_obj.channellayout_original:
-            channel_positions = mi_obj.channellayout_original
+        if a_track.channellayout_original:
+            channel_positions = a_track.channellayout_original
 
         return channel_positions
 
     @staticmethod
-    def get_channels(mi_object):
+    def get_channels(a_track: Track) -> int:
         """
         Get the number of audio channels for the specified track.
 
@@ -40,15 +43,15 @@ class ParseAudioChannels:
         of the highest potential channel count.
 
         Args:
-            mi_object (MediaInfo): A MediaInfo object containing information about the media
+            mi_object (Track): MediaInfo track object containing information about the media
             file's audio track.
 
         Returns:
             The number of audio channels as an integer.
         """
-        base_channels = mi_object.channel_s
-        check_other = re.search(r"\d+", str(mi_object.other_channel_s[0]))
-        check_other_2 = str(mi_object.channel_s__original)
+        base_channels = a_track.channel_s
+        check_other = re.search(r"\d+", str(a_track.other_channel_s[0]))
+        check_other_2 = str(a_track.channel_s__original)
 
         # Create a list of values to find the maximum
         values = [int(base_channels)]
