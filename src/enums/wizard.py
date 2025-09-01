c9b812c1074f93655f222b6ab60b75766d167939
jesterr0 <jlw_4049@proton.me>
2025-08-31 21:22:45 -0400

    refactor: update page enums refactor: override string method

--- src/enums/wizard.py
+++ src/enums/wizard.py
@@ -1,14 +1,21 @@
 from enum import Enum, auto as auto_enum
 
+from typing_extensions import override
+
 
 class WizardPages(Enum):
-    BASIC_INPUT_PAGE = auto_enum()
-    ADVANCED_INPUT_PAGE = auto_enum()
+    INPUT_PAGE = auto_enum()
     PLUGIN_INPUT_PAGE = auto_enum()
     MEDIA_SEARCH_PAGE = auto_enum()
+    SERIES_MATCHER_PAGE = auto_enum()
     RENAME_ENCODE_PAGE = auto_enum()
+    RENAME_ENCODE_SERIES_PAGE = auto_enum()
     IMAGES_PAGE = auto_enum()
     TRACKERS_PAGE = auto_enum()
     RELEASE_NOTES_PAGE = auto_enum()
     NFO_TEMPLATE_PAGE = auto_enum()
     PROCESS_PAGE = auto_enum()
+
+    @override
+    def __str__(self) -> str:
+        return self.name.replace("_", " ").title()
