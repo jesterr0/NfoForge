1e69288e37742940f58048989b1df08f2853898d
jesterr0 <jlw_4049@proton.me>
2025-10-26 13:44:44 -0400

    refactor: just use auto

--- src/enums/wizard.py
+++ src/enums/wizard.py
@@ -1,20 +1,20 @@
-from enum import Enum, auto as auto_enum
+from enum import Enum, auto
 
 from typing_extensions import override
 
 
 class WizardPages(Enum):
-    INPUT_PAGE = auto_enum()
-    PLUGIN_INPUT_PAGE = auto_enum()
-    MEDIA_SEARCH_PAGE = auto_enum()
-    SERIES_MATCHER_PAGE = auto_enum()
-    RENAME_ENCODE_PAGE = auto_enum()
-    # RENAME_ENCODE_SERIES_PAGE = auto_enum()
-    IMAGES_PAGE = auto_enum()
-    TRACKERS_PAGE = auto_enum()
-    RELEASE_NOTES_PAGE = auto_enum()
-    NFO_TEMPLATE_PAGE = auto_enum()
-    PROCESS_PAGE = auto_enum()
+    INPUT_PAGE = auto()
+    PLUGIN_INPUT_PAGE = auto()
+    MEDIA_SEARCH_PAGE = auto()
+    SERIES_MATCHER_PAGE = auto()
+    RENAME_ENCODE_PAGE = auto()
+    # RENAME_ENCODE_SERIES_PAGE = auto()
+    IMAGES_PAGE = auto()
+    TRACKERS_PAGE = auto()
+    RELEASE_NOTES_PAGE = auto()
+    NFO_TEMPLATE_PAGE = auto()
+    PROCESS_PAGE = auto()
 
     @override
     def __str__(self) -> str:
