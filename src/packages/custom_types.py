db2570eecc4e1cd6792ecb3e0e2b10e7e2fba71f
jesterr0 <jesterr0@proton.me>
2026-07-20 00:26:41 -0400

    refactor: convert to class based

--- src/packages/custom_types.py
+++ src/packages/custom_types.py
@@ -3,33 +3,42 @@ from typing import NamedTuple
 
 from src.enums.image_host import ImageHost, ImageSource
 
-SubNames = NamedTuple("SubNames", [("source", str), ("encode", str)])
-CropValues = NamedTuple(
-    "CropValues", [("top", int), ("bottom", int), ("left", int), ("right", int)]
-)
-AdvancedResize = NamedTuple(
-    "AdvancedResize",
-    [
-        ("src_left", float),
-        ("src_top", float),
-        ("src_width", float),
-        ("src_height", float),
-    ],
-)
-ImageUploadData = NamedTuple(
-    "ImageUploadData", [("url", str | None), ("medium_url", str | None)]
-)
-ImageUploadFromTo = NamedTuple(
-    "ImageUploadFromTo",
-    [("img_from", ImageSource), ("img_to", ImageSource | ImageHost)],
-)
-RenameNormalization = NamedTuple(
-    "RenameNormalization",
-    (
-        ("normalized", str),
-        ("re_gex", tuple[str]),
-    ),
-)
-ComparisonPair = NamedTuple(
-    "ComparisonPair", (("source", Path), ("media", Path), ("script", Path | None))
-)
+
+class SubNames(NamedTuple):
+    source: str
+    encode: str
+
+
+class CropValues(NamedTuple):
+    top: int
+    bottom: int
+    left: int
+    right: int
+
+
+class AdvancedResize(NamedTuple):
+    src_left: float
+    src_top: float
+    src_width: float
+    src_height: float
+
+
+class ImageUploadData(NamedTuple):
+    url: str | None
+    medium_url: str | None
+
+
+class ImageUploadFromTo(NamedTuple):
+    img_from: ImageSource
+    img_to: ImageSource | ImageHost
+
+
+class RenameNormalization(NamedTuple):
+    normalized: str
+    re_gex: tuple[str, ...]
+
+
+class ComparisonPair(NamedTuple):
+    source: Path
+    media: Path
+    script: Path | None
